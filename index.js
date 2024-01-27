0b995789dc1db2ec12a0558ad93e93d85c767673
AK-Rasel <raselak101010@gmail.com>
2024-01-27 12:53:43 +0600

    clean co server ready and mongodb added

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = 5000;
+// console.log(process.env.SECRET_USER_NAME);
+// CleanCo
+// OpeQD6cCSyo9nL3q
+
+// db url
+
+// const uri =
+//   "mongodb+srv://<username>:<password>@cluster0.z9hqskk.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.SECRET_USER_NAME}:${process.env.SECRET_PASS}@cluster0.z9hqskk.mongodb.net/CleanCo?retryWrites=true&w=majority`;
+//   connet mongodb fun
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // collaction
+    // const servicesCollection = client.db("CleanCo").collection("services");
+
+    // see mongodb
+    // app.get("/api/v1/services", async (req, res) => {
+    //   const cursor = servicesCollection.find();
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Clean-Co-Server Running");
+});
+
+app.listen(port, () => {
+  console.log(`Clean-co-server listening on port ${port}`);
+});
